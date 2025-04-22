ce5d23c253435ba1891407dae805d89d7a3f3176
Vishal Omprasad <vishal.omprasad@syncfusion.com>
2025-04-23 00:38:44 +0530

    Updated .NET version to .NET9.0

---
+++ MapsExample/MapsExample/Model/Model.cs
@@ -0,0 +1,17 @@
+﻿namespace MapsExample
+{
+    public class Model
+    {
+        public double Value { get; set; }
+        public string Country { get; set; }
+        public double Population { get; set; }
+        public double Density { get; set; }
+        public Model(double value, string country, double population, double density)
+        {
+            Value = value;
+            Country = country;
+            Population = population;
+            Density = density;
+        }
+    }
+}
